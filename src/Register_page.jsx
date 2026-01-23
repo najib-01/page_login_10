5556bbefea34180494a58a880d404c27409262fa
najib-01 <rnajib198@gmail.com>
2026-01-23 08:20:59 +0100

    remove motion

--- src/Register_page.jsx
+++ src/Register_page.jsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faTwitter, faGooglePlusG } from "@fortawesome/free-brands-svg-icons";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 
 export default function Register_page(){
